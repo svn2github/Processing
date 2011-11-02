4965b90ee9bab178ada85d4021504ebf2eeec339
prisonerjohn@gmail.com <prisonerjohn@gmail.com@fe3ceab9-de17-3b67-bb2e-7d58edd5e4d6>
2011-11-02 18:42:15 +0000

    Library Template: Changed the placeholder keywords for clarity, and fixed the example generation target.
    git-svn-id: http://processing.googlecode.com/svn/trunk@8817 fe3ceab9-de17-3b67-bb2e-7d58edd5e4d6

--- library-template/src/template/library/HelloLibrary.java
+++ library-template/src/template/library/HelloLibrary.java
@@ -1,7 +1,9 @@
 /**
- * you can put a one sentence description of your library here.
+ * ##library.name##
+ * ##library.sentence##
+ * ##library.url##
  *
- * ##copyright##
+ * Copyright ##copyright## ##author##
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -18,9 +20,9 @@
  * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
  * Boston, MA  02111-1307  USA
  * 
- * @author		##author##
- * @modified	##date##
- * @version		##version##
+ * @author      ##author##
+ * @modified    ##date##
+ * @version     ##library.prettyVersion## (##library.version##)
  */
 
 package template.library;
@@ -31,7 +33,7 @@ import processing.core.*;
 /**
  * This is a template class and can be used to start a new processing library or tool.
  * Make sure you rename this class as well as the name of the example package 'template' 
- * to your own lobrary or tool naming convention.
+ * to your own library or tool naming convention.
  * 
  * @example Hello 
  * 
@@ -47,7 +49,7 @@ public class HelloLibrary {
 
 	int myVariable = 0;
 	
-	public final static String VERSION = "##version##";
+	public final static String VERSION = "##library.prettyVersion##";
 	
 
 	/**
@@ -64,7 +66,7 @@ public class HelloLibrary {
 	
 	
 	private void welcome() {
-		System.out.println("##name## ##version## by ##author##");
+		System.out.println("##library.name## ##library.prettyVersion## by ##author##");
 	}
 	
 	
